ee79c9b83b35ab22958a64a7b70e2f60aa6d6ed0
Ade Setiawan <adesetiawan0675@gmail.com>
2020-03-26 11:45:29 +0700

    PHP

---
+++ akademik.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 24, 2020 at 11:23 PM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `akademik`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dt_prodi`
+--
+
+CREATE TABLE `dt_prodi` (
+  `idprodi` int(11) NOT NULL,
+  `kdprodi` varchar(10) NOT NULL,
+  `nmprodi` varchar(70) NOT NULL,
+  `akreditasi` enum('A','B','C','-') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `dt_prodi`
+--
+
+INSERT INTO `dt_prodi` (`idprodi`, `kdprodi`, `nmprodi`, `akreditasi`) VALUES
+(1, '2099', 'Pariwisata', '-'),
+(2, '3061', 'Manajemen Informatika', 'B'),
+(3, '3020', 'Akuntansi', 'A');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `dt_prodi`
+--
+ALTER TABLE `dt_prodi`
+  ADD PRIMARY KEY (`idprodi`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `dt_prodi`
+--
+ALTER TABLE `dt_prodi`
+  MODIFY `idprodi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
